389e7a3fac2f574390650fba41107a6ded23ea18
code_egg <louis@pygamestudio.com>
2026-05-03 13:42:20 +0800

    Update main_template.py

--- src/pygamestudio/common/res/templates/main_template.py
+++ src/pygamestudio/common/res/templates/main_template.py
@@ -1,31 +1,33 @@
 import os
 import sys
-sys.path.insert(0, 'D:\\Github\\pygamestudio\\src')
-
 import pygame
 from pathlib import Path
-from pygamestudio.api.runtime.config import get_project_config
 from pygamestudio.api.runtime.scene import load_scene
+from pygamestudio.api.runtime.config import get_project_config
 
-PROJECT_PATH = Path(__file__).parent.resolve().as_posix()
-os.environ['PROJECT_PATH'] = PROJECT_PATH
-
-
-pygame.init()
+# setup pygamestudio project env to get the project config 
+os.environ['PROJECT_PATH'] = Path(__file__).parent.resolve().as_posix()
 PROJECT_CONFIG = get_project_config()
+
+# pygame setup
+pygame.init()
 pygame.display.set_caption(PROJECT_CONFIG['caption'])
 screen = pygame.display.set_mode(PROJECT_CONFIG['screen_size'])
 clock = pygame.time.Clock()
 
-
 running = True
 while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    # load the scene created in pygamestudio
+    # if no scene_path is provided, the default current_scene from the project config will be loaded
     load_scene(screen)
 
+    # flip() the display to put your work on screen and limits FPS to 60
     pygame.display.flip()
     clock.tick(60)
 
