13bdd501656d01f544372296752a67f2dfe3e748
code_egg <louis@pygamestudio.com>
2026-05-03 19:19:58 +0800

    Update main.py

--- src/pygamestudio/main.py
+++ src/pygamestudio/main.py
@@ -23,13 +23,16 @@ SOFTWARE.
 """
 
 import sys
-sys.path.insert(0, 'D:\\Github\\pygamestudio\\src')
 from PySide6.QtWidgets import QApplication
 from pygamestudio.gui.main import PygameStudio
 
 
-if __name__ == '__main__':
+def main():
     app = QApplication([])
     pygame_studio = PygameStudio()
     pygame_studio.start()
     sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
